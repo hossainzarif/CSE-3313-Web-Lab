75eab4779d895acb0c88b3e61e5333b0aab04336
hossainzarif <zarifh493@gmail.com>
2022-09-30 20:35:03 +0600

    all at once

---
+++ index.js
@@ -0,0 +1,18 @@
+// Ecommerce (fruits, veg, biscuits )
+// remove (null)
+
+var isBool = true
+
+if (isBool) {
+  var varVariable = 'Hello World'
+  isBool = false
+}
+
+if (isBool) {
+  let myVar = 10
+} else {
+  var anotherVar = 10
+  let myVar = anotherVar + 15
+}
+// console.log(myVar)   // Console log 1
+console.log(varVariable) // Console log 2
